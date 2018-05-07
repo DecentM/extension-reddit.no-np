7dc5e99d0dbf5c716fccc71ea374383dc2f41cb1
DecentM <decentm@decentm.com>
2018-05-07 08:47:40 +0200

    feat: Process injected links + better link note

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,61 +1,88 @@
+import assert from 'assert'
+
 ((window, document, undefined) => new Promise((resolve, reject) => {
   'use strict';
+  
+  const handle = (error) => {
+    console.error('An error occurred in reddit.no-np:')
+    reject(error)
+  }
+  
+  try {
+    const changeLink = ($link) => {
+      const rx = /np\.reddit\.com/gi;
+      
+      $link.href = $link.href.replace(rx, 'reddit.com');
+      $link.classList.add('decentm-np-replaced');
+    };
+    
+    const log = (text) => {
+      console.log(
+        `%c ${text} %c https://github.com/DecentM/extension-reddit.no-np`,
+        [
+          'color: white;',
+          'padding: 5px;',
+          'padding-left: 7px;',
+          'padding-right: 7px;',
+          'background: linear-gradient(130deg, #000FE5, #005FFF);',
+          'border-radius: 3px;',
+          'margin-right: 16px;',
+        ].join(''),
+        [
+          'margin-bottom: 16px;',
+          'margin-top: 16px;',
+        ].join('')
+      );
+    };
 
-  let changedLinks = 0;
-  const changeLink = ($link) => {
-    const rx = /np\.reddit\.com/gi;
+    const changeLinks = ($target) => new Promise((resolve, reject) => {
+      let changedLinks = 0;
 
-    $link.href = $link.href.replace(rx, 'reddit.com');
-    $link.classList.add('decentm-np-replaced');
+      if (!($target instanceof HTMLElement)) {
+        $target = document.body;
+      }
 
-    changedLinks = changedLinks + 1;
-  };
-
-  const log = (text) => {
-    console.log(
-      `%c ${text} %c https://github.com/DecentM/extension-reddit.no-np`,
-      [
-        'color: white;',
-        'padding: 5px;',
-        'padding-left: 7px;',
-        'padding-right: 7px;',
-        'background: linear-gradient(130deg, #000FE5, #005FFF);',
-        'border-radius: 3px;',
-        'margin-right: 16px;',
-      ].join(''),
-      [
-        'margin-bottom: 16px;',
-        'margin-top: 16px;',
-      ].join('')
-    );
-  };
-
-  window.addEventListener('load', () => {
-    const $links = Array.prototype.slice.call(document.getElementsByTagName('a'));
-    const $validLinks = $links.filter(($link) => $link.href.includes('np.reddit.com'));
-
-    $validLinks.forEach(($link, index) => {
-      requestIdleCallback(() => {
-        changeLink($link);
-
-        if (index + 1 === $validLinks.length) {
-          log(`Removed no-participation subdomain from ${changedLinks} link(s)`);
-        }
+      const $links = Array.prototype.slice.call($target.getElementsByTagName('a'));
+      const $validLinks = $links.filter(($link) => $link.href.includes('np.reddit.com'));
+      
+      observer.observe(document.body, {
+        'subtree':   true,
+        'childList': true,
       });
-    });
-  });
+      
+      $validLinks.forEach(($link, index) => {
+        requestIdleCallback(() => {
+          changeLink($link);
+          changedLinks = changedLinks + 1;
+          
+          if (index + 1 === $validLinks.length) {
+            log(`Removed no-participation subdomain from ${changedLinks} link(s)`);
+            changedLinks = 0;
+          }
+        });
+      });
+    })
 
-  const $stylesheet = document.createElement('style');
-
-  $stylesheet.type = 'text/css';
-  $stylesheet.id = 'decentm-np-replacer-css';
-
-  $stylesheet.innerHTML = `
+    const mutationHandler = (mutationList) => {
+      mutationList.forEach((mutation) => {
+        changeLinks(mutation.target)
+      })
+    };
+    const observer = new MutationObserver(mutationHandler)
+    
+    window.addEventListener('load', changeLinks);
+    
+    const $stylesheet = document.createElement('style');
+    
+    $stylesheet.type = 'text/css';
+    $stylesheet.id = 'decentm-np-replacer-css';
+    
+    $stylesheet.innerHTML = `
     .decentm-np-replaced::after {
       transition:
-        width .25s cubic-bezier(.56,.01,.44,1),
-        clip-path .25s cubic-bezier(.56,.01,.44,1) !important;
-
+      width .25s cubic-bezier(.56,.01,.44,1),
+      clip-path .25s cubic-bezier(.56,.01,.44,1) !important;
+      
       display: inline-block !important;
       content: "(-NP)" !important;
       width: 0 !important;
@@ -68,14 +95,18 @@
       clip-path: polygon(0 0, 0 0, 0 100%, 0% 100%) !important;
       margin-left: 0;
     }
-
+    
     .decentm-np-replaced:hover::after {
       width: 45px !important;
       height: 19px !important;
       clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%) !important;
       margin-left: 8px;
     }
-  `;
-
-  document.querySelector('body').appendChild($stylesheet);
+    `;
+    
+    document.querySelector('body').appendChild($stylesheet);
+    resolve()
+  } catch (error) {
+    handle(error)
+  }
 }))(window, window.document);
