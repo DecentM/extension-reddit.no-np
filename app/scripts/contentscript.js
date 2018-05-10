636ae891902e8cbcc81aa82867fe0017ec67a8c4
DecentM <decentm@decentm.com>
2018-05-11 00:09:57 +0200

    feat: Show on-page notification, only if 10+ links replaced

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,5 @@
+import {debounce,} from 'lodash'
+
 ((window, document, undefined) => new Promise((resolve, reject) => {
   'use strict';
   
@@ -13,8 +15,32 @@
       $link.href = $link.href.replace(rx, 'reddit.com');
       $link.classList.add('decentm-np-replaced');
     };
+    let changedLinks = 0;
     
-    const log = (text) => {
+    const notification = (text) => {
+      const $notification = document.createElement('div');
+      
+      $notification.classList.add('decentm-np-notification');
+      $notification.innerHTML = text;
+
+      document.body.appendChild($notification);
+      setTimeout(() => {
+        $notification.classList.add('show')
+      }, 1);
+
+      setTimeout(() => {
+        $notification.classList.remove('show');
+
+        setTimeout(() => {
+          $notification.remove();
+        }, 500);
+      }, 4000);
+      
+      changedLinks = 0;
+    }
+    
+    const log = debounce((text) => {
+      notification(text);
       console.log(
         `%c ${text} %c https://github.com/DecentM/extension-reddit.no-np`,
         [
@@ -31,11 +57,9 @@
           'margin-top: 16px;',
         ].join('')
       );
-    };
+    }, 400);
 
     const changeLinks = ($target) => new Promise((resolve, reject) => {
-      let changedLinks = 0;
-
       if (!($target instanceof HTMLElement)) {
         $target = document.body;
       }
@@ -54,8 +78,9 @@
           changedLinks = changedLinks + 1;
           
           if (index + 1 === $validLinks.length) {
-            log(`Removed no-participation subdomain from ${changedLinks} link(s)`);
-            changedLinks = 0;
+            if (changedLinks > 10) {
+              log(`Removed no-participation subdomain from ${changedLinks} link(s)`);
+            }
           }
         });
       });
@@ -67,9 +92,6 @@
       })
     };
     const observer = new MutationObserver(mutationHandler)
-    
-    window.addEventListener('load', changeLinks);
-    
     const $stylesheet = document.createElement('style');
     
     $stylesheet.type = 'text/css';
@@ -100,9 +122,44 @@
       clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%) !important;
       margin-left: 8px;
     }
+    
+    .decentm-np-notification {
+      padding-top: 16px;
+      padding-bottom: 16px;
+      padding-left: 24px;
+      padding-right: 24px;
+      bottom: 2rem;
+      right: 2rem;
+      background: linear-gradient(130deg, #000FE5, #005FFF);
+      font-size: 14px;
+      color: white;
+      border-radius: 3px;
+
+      position: fixed;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      user-select: none;
+      z-index: 99999999999;
+      
+      opacity: 0;
+      transform: scale(.9);
+      pointer-events: none;
+      
+      transition-property: transform, opacity;
+      transition-duration: .3s;
+      transition-timing-function: ease-in-out;
+    }
+    
+    .decentm-np-notification.show {
+      opacity: 1;
+      transform: scale(1);
+      pointer-events: all;
+    }
     `;
     
     document.querySelector('body').appendChild($stylesheet);
+    changeLinks()
     resolve()
   } catch (error) {
     handle(error)
